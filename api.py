756b73c0414ac23ba69853ad63c83d44847dd42d
lilagrc <lilagrc@gmail.com>
2015-10-19 23:11:53 +0000

    adds put request for api

--- api.py
+++ api.py
@@ -1,4 +1,12 @@
 import requests
+import json
+
+
+def send_confirmation():
+  url = 'http://www.robofeedpet.com/pi_confirmation'
+  payload = {'request': 'success'}
+  r = requests.put(url, json=payload)
+  return
 
 # r = requests.put('http://www.robofeedpet.com/run_pi', data = {"feed":"yes"})
 r = requests.get('http://www.robofeedpet.com/run_pi')
@@ -7,8 +15,14 @@ r = r.json()
 print r
 
 if r["request"] == "feed":
-  import servo1
+  import servo1;
+  send_confirmation();
 else:
   print "No food now"
+
+
+
+
+
 
 
