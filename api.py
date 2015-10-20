c5199cc4b2405f7783b154678cb25302dfcbbd3f
lilagrc <lilagrc@gmail.com>
2015-10-20 18:37:36 +0000

    updated scripts

--- api.py
+++ api.py
@@ -1,22 +1,27 @@
 import requests
 import json
 
+from requests.auth import HTTPBasicAuth
+
 
 def send_confirmation():
-  url = 'http://www.robofeedpet.com/pi_confirmation'
+  url = 'http://www.robofeedpet.com/api/v1/requests/confirmation'
   payload = {'request': 'success'}
   r = requests.put(url, json=payload)
   return
 
 # r = requests.put('http://www.robofeedpet.com/run_pi', data = {"feed":"yes"})
-r = requests.get('http://www.robofeedpet.com/run_pi')
+url = 'http://www.robofeedpet.com/api/v1/requests/new'
+headers = {'Authorization': 'Token token="EDlux0E1KtmRT6YDvQRTvQtt"'}
+
+r = requests.get(url, headers=headers)
 
 r = r.json()
 print r
 
 if r["request"] == "feed":
   import servo1;
-  send_confirmation();
+  #send_confirmation();
 else:
   print "No food now"
 
