fd37ce1ac8a09b7c5e05a1827d828ff59b59d44b
lilagrc <lilagrc@gmail.com>
2015-10-21 19:11:45 +0000

    Working on scheduled feedings

--- api.py
+++ api.py
@@ -1,28 +1,31 @@
 import requests
 import json
 
-from requests.auth import HTTPBasicAuth
+# from requests.auth import HTTPBasicAuth
 
 
 def send_confirmation():
   url = 'http://www.robofeedpet.com/api/v1/requests/confirmation'
-  headers = {'Authorization': 'Token token="EDlux0E1KtmRT6YDvQRTvQtt"'}
+  headers = {'Authorization': 'Token token="rl69KLMDARz3xBdQy3valQtt"'}
   payload = {'request': 'success'}
   r = requests.put(url, json=payload, headers=headers)
   return
 
 # r = requests.put('http://www.robofeedpet.com/run_pi', data = {"feed":"yes"})
 url = 'http://www.robofeedpet.com/api/v1/requests/new'
-headers = {'Authorization': 'Token token="EDlux0E1KtmRT6YDvQRTvQtt"'}
+headers = {'Authorization': 'Token token="rl69KLMDARz3xBdQy3valQtt"'}
 
 r = requests.get(url, headers=headers)
 
 r = r.json()
 print r
 
-if r["request"] == "feed":
+if r["feed_request"] == "feed" and r["schedule_request"] == None:
   import servo1;
   send_confirmation();
+elif r["schedule_request"] and r["feed_request"] == None:
+ # Need to add logic to deal with times that are sent, which trigger scripts
+ print "we're getting there"
 else:
   print "No food now"
 
