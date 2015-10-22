6a16541d38c4d8e21b5672488dab89b12843c1d0
lilagrc <lilagrc@gmail.com>
2015-10-22 11:43:28 -0700

    progress on timed feedings

--- api.py
+++ api.py
@@ -22,29 +22,55 @@ r = requests.get(url, headers=headers)
 r = r.json()
 print r
 
-
-if r["feed_request"] == "feed" and r["schedule_request"] == None:
+if r["feed_request"] == None and r["schedule_request"] == None:
+  print "do nothing"
+elif r["feed_request"] == "feed" and r["schedule_request"] == None:
   import servo1;
   send_confirmation();
-elif r["schedule_request"] and r["feed_request"] == None:
+elif r["schedule_request"] != "cancel" and r["feed_request"] == None:
   time = r["schedule_request"]
   time = int(time)
-  tab = CronTab(user=True)
-  cmd = 'sh /home/pi/schedule_launcher.sh'
-  cron_job = tab.new(cmd)
-  cron_job.minute.on(0)
-  cron_job.hour.on(time)
 
-  tab.write()
+  cron = CronTab(user='root')
+  cmd = 'sh /home/pi/schedule_launcher.sh'
+
+  duplicate = None
+
+  for job in cron:
+    if job.command == cmd:
+      duplicate = "yes"
+
+  if duplicate != "yes":
+    new_job = cron.new(cmd)
+    new_job.minute.on(0)
+    new_job.hour.on(time)
+    cron.write()
+
+
+  # go through and see if there is a match
+  #   job.minute.on(0)
+  #   job.hour.on(time)
+  #   cron.write()
+  #   print "Am I here?"
+
 elif r["schedule_request"] == "cancel":
-  tab = CronTab(user=True)
+  tab = CronTab(user='root')
   cmd = 'sh /home/pi/schedule_launcher.sh'
 
   cron_job = tab.find_command(cmd)
   tab.remove_all(cmd)
   tab.write()
+  print "or am i deleting?"
 else:
   print "No food now"
+
+
+#* * * * * sleep 00; sh /home/pi/launcher.sh
+#* * * * * sleep 15; sh /home/pi/launcher.sh
+#* * * * * sleep 30; sh /home/pi/launcher.sh
+#* * * * * sleep 45; sh /home/pi/launcher.sh
+
+
 
 
 
